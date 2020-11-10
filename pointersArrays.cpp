3c57803a20cd5f4a0959b7b36a9487c1e01249df
Reo Yamanaka <ryamanaka807@gmail.com>
2020-11-09 20:45:32 -0500

    added last element of array elaboration

--- pointersArrays.cpp
+++ pointersArrays.cpp
@@ -18,14 +18,22 @@ int main(){
     printf("%d ", myArray[i]);
   }
   printf("]\n\n");
-  printf("Memory address of first element: %p\n", myPointer);
-  printf("An equivalent way to print the memory address of first element: %p\n\n", myPointer);
+  printf("Memory address of first element: %p\n", myArray);
+  printf("An equivalent way to print the memory address of first element: %p\n", myPointer);
+  printf("Yet another way to print the memory address of first element: %p\n\n", myArray);
 
   cout << "The lines below print the actual value of the first element of the array 'myArray': " << endl;
   printf("\nValue of first element: %d\n", *myPointer);
-  printf("An equivalent way to print the value of the first element: %d\n\n", myArray[0]);
+  printf("An equivalent way to print the value of the first element: %d\n", myArray[0]);
+  printf("Yet another way to print the value of the first element: %d\n\n", *myArray);
 
+  cout << "Below I do the same thing for the last element of myArray:" << endl << endl;
+  printf("Memory address of last element of myArray: %p\n", myPointer + myArraySize);
+  printf("Memory address of last element of myArray: %p\n\n", myArray + myArraySize);
 
+  printf("Value of last element of myArray: %d\n", *(myPointer+myArraySize - 1));
+  printf("Value of last element of myArray: %d\n", myArray[myArraySize-1]);
+  printf("Value of last element of myArray: %d\n", *(myArray + myArraySize - 1));
 
   return 0;
 }
